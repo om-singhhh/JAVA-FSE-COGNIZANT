7d323c335eddfabdfa3bae1ef87bbaea54e2221e
OmSingh04 <omsingh18609@gmail.com>
2026-06-28 15:46:13 +0530

    Implementing the singleTon pattern.

---
+++ Design_Patterns_and_Principles/SingleTonPattern/SingletonTest.java
@@ -0,0 +1,19 @@
+package Design_Patterns_and_Principles.SingleTonPattern;
+
+public class SingletonTest {
+     public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("User Logged In");
+
+        System.out.println(logger1.hashCode());
+        System.out.println(logger2.hashCode());
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        }
+    }
+}
